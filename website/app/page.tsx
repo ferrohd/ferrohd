8a45d9a7cd7ddfcc46159acd6e77a094c220eb2b
Alessandro <aleferrara1998@gmail.com>
2024-03-11 18:33:18 +0100

    ad bio and fix pagination

--- website/app/page.tsx
+++ website/app/page.tsx
@@ -15,7 +15,7 @@ export default function Home() {
           </div>
         </div>
         <nav className="flex mx-auto gap-4 sm:gap-6 pt-6">
-          <Link className="text-lg font-medium hover:underline underline-offset-4" href="#">
+          <Link className="text-lg font-medium hover:underline underline-offset-4" href="#bio">
             Bio
           </Link>
           <Link className="text-lg font-medium hover:underline underline-offset-4" href="#skills">
@@ -33,30 +33,20 @@ export default function Home() {
         </nav>
       </header>
       <main className="flex-1">
-        <section className="w-full py-12 md:py-24 lg:py-32 h-screen">
-          <div className="container mx-auto flex flex-col items-center justify-center gap-4 px-4 md:px-6 lg:gap-10">
-            <div className="space-y-3 text-center">
-              <h1 className="text-3xl font-bold tracking-tighter sm:text-6xl md:text-6xl/none">Hi, I&apos;m Ferro👋</h1>
-              <p className="text-gray-500 dark:text-gray-400">Nice to meet you.</p>
-            </div>
-            <div className="mx-auto w-full max-w-prose space-y-2">
-              <p className="text-base text-gray-500 md:text-xl/relaxed dark:text-gray-400">
-                I&apos;m a backend engineer with a passion for building scalable and efficient systems. My expertise lies in
-                designing and optimizing databases, crafting robust APIs, and ensuring seamless integration between
-                services. I love diving into complex problems and finding elegant solutions.
-              </p>
-            </div>
+
+        <section className="w-full py-12 md:py-24 lg:py-32 flex">
+          <div className="m-auto text-center">
+            <h1 className="text-3xl font-bold tracking-tighter sm:text-6xl md:text-6xl/none">Ciao, I&apos;m Ferro 👋</h1>
+            <p className="text-gray-500 dark:text-gray-400">Nice to meet you.</p>
           </div>
         </section>
 
-        <section className="w-full py-12 md:py-24 lg:py-32">
+        <section id="bio" className="w-full py-12 md:py-24 lg:py-32">
           <div className="container mx-auto grid items-start gap-6 px-4 md:px-6 lg:grid-cols-2 lg:gap-10">
             <div className="space-y-4">
               <h2 className="text-3xl font-bold tracking-tighter sm:text-4xl xl:text-5xl/none">About Me</h2>
               <p className="max-w-[600px] text-gray-500 md:text-xl/relaxed lg:text-base/relaxed xl:text-xl/relaxed dark:text-gray-400">
-                I&apos;m a backend engineer with a passion for building scalable and efficient systems. My expertise lies in
-                designing and optimizing databases, crafting robust APIs, and ensuring seamless integration between
-                services. I love diving into complex problems and finding elegant solutions. When I&apos;m not coding, I&apos;m riding my bike as fast as my code.
+                I&apos;m an Italian engineer with an MSc in Computer Science and Engineering specialized in building scalable and efficient systems. My expertise lies in designing and optimizing databases, crafting robust APIs, and ensuring seamless integration between services. I love diving into complex problems and finding elegant solutions. When I&apos;m not coding, I&apos;m riding my bike as fast as my code.
               </p>
             </div>
             <div className="mx-auto flex items-center justify-center rounded-lg overflow-hidden w-full max-w-md">
@@ -82,33 +72,15 @@ export default function Home() {
 
       {/** Footer */}
       <footer className="flex flex-col gap-2 sm:flex-row py-6 w-full shrink-0 items-center px-4 md:px-6">
-        <p className="text-xs text-gray-500 dark:text-gray-400">© 2024 Acme Inc. All rights reserved.</p>
+        <p className="text-xs text-gray-500 dark:text-gray-400">This website is spaghetti with no sauce.</p>
         <nav className="sm:ml-auto flex gap-4 sm:gap-6">
+          {/*
           <Link className="text-xs hover:underline underline-offset-4" href="#">
             Privacy
           </Link>
+          */}
         </nav>
       </footer>
     </div>
-  )
-}
-
-function CheckCircleIcon(props: JSX.IntrinsicAttributes & SVGProps<SVGSVGElement>) {
-  return (
-    <svg
-      {...props}
-      xmlns="http://www.w3.org/2000/svg"
-      width="24"
-      height="24"
-      viewBox="0 0 24 24"
-      fill="none"
-      stroke="currentColor"
-      strokeWidth="2"
-      strokeLinecap="round"
-      strokeLinejoin="round"
-    >
-      <path d="M22 11.08V12a10 10 0 1 1-5.93-9.14" />
-      <polyline points="22 4 12 14.01 9 11.01" />
-    </svg>
   )
 }
