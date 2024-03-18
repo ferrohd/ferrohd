5ffb060b73f57282da7e7b506b264c76b628a9f6
Alessandro <aleferrara1998@gmail.com>
2024-03-18 11:03:28 +0100

    add link to repo

--- website/app/page.tsx
+++ website/app/page.tsx
@@ -65,7 +65,7 @@ export default function Home() {
 
       {/** Footer */}
       <footer className="flex flex-col gap-2 sm:flex-row py-6 w-full shrink-0 items-center px-4 md:px-6">
-        <p className="text-xs text-gray-500 dark:text-gray-400">This website is spaghetti with no sauce.</p>
+        <p className="text-xs text-gray-500 dark:text-gray-400">This website is spaghetti with <Link className="underline" href="https://github.com/ferrohd/ferrohd/" target="_blank">the sauce</Link>.</p>
         <nav className="sm:ml-auto flex gap-4 sm:gap-6">
           {/*
           <Link className="text-xs hover:underline underline-offset-4" href="#">
