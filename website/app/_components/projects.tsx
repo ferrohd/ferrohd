acbf87f5c8da1763a18c2847f416b707e429238e
Alessandro <aleferrara1998@gmail.com>
2024-03-18 09:50:52 +0100

    move sections outside component

--- website/app/_components/projects.tsx
+++ website/app/_components/projects.tsx
@@ -31,33 +31,31 @@ export default function Projects() {
     ];
 
     return (
-        <section id="projects" className="w-full py-12 md:py-24 lg:py-32">
-            <div className="container  mx-auto grid gap-6 px-4 md:gap-10 md:px-6">
-                <div className="space-y-3">
-                    <h2 className="text-3xl font-bold tracking-tighter sm:text-4xl xl:text-5xl/none">Stuff I&apos;ve Built</h2>
-                    <p className="text-gray-500 dark:text-gray-400">Talk is cheap. Show me the code. <i>-Linus Torvalds</i></p>
-                </div>
-                <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-x-6 gap-y-12">
-                    {
-                        projects.map((project, index) => (
-                            <div key={index} className="space-y-4">
-                                <h3 className="text-2xl font-bold tracking-tight">{project.title}</h3>
-                                <p className="max-w-prose text-gray-500 md:text-base/relaxed dark:text-gray-400">{project.description}</p>
-                                <Link
-                                    className="inline-flex h-10 items-center justify-center rounded-md border border-gray-200 bg-white px-8 text-sm font-medium shadow-sm gap-1 transition-colors hover:bg-gray-100 hover:text-gray-900 focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-gray-950 disabled:pointer-events-none disabled:opacity-50 dark:border-gray-800 dark:bg-gray-950 dark:hover:bg-gray-800 dark:hover:text-gray-50 dark:focus-visible:ring-gray-300"
-                                    href={project.link}
-                                    target="_blank"
-                                >
-                                    View Project
-                                    <ArrowRightIcon className="h-4 w-4 peer-ml" />
-                                </Link>
-                            </div>
-                        ))
-                    }
-
-                </div>
+        <div className="container  mx-auto grid gap-6 px-4 md:gap-10 md:px-6">
+            <div className="space-y-3">
+                <h2 className="text-3xl font-bold tracking-tighter sm:text-4xl xl:text-5xl/none">Stuff I&apos;ve Built</h2>
+                <p className="text-gray-500 dark:text-gray-400">Talk is cheap. Show me the code. <i>-Linus Torvalds</i></p>
             </div>
-        </section>
+            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-x-6 gap-y-12">
+                {
+                    projects.map((project, index) => (
+                        <div key={index} className="space-y-4">
+                            <h3 className="text-2xl font-bold tracking-tight">{project.title}</h3>
+                            <p className="max-w-prose text-gray-500 md:text-base/relaxed dark:text-gray-400">{project.description}</p>
+                            <Link
+                                className="inline-flex h-10 items-center justify-center rounded-md border border-gray-200 bg-white px-8 text-sm font-medium shadow-sm gap-1 transition-colors hover:bg-gray-100 hover:text-gray-900 focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-gray-950 disabled:pointer-events-none disabled:opacity-50 dark:border-gray-800 dark:bg-gray-950 dark:hover:bg-gray-800 dark:hover:text-gray-50 dark:focus-visible:ring-gray-300"
+                                href={project.link}
+                                target="_blank"
+                            >
+                                View Project
+                                <ArrowRightIcon className="h-4 w-4 peer-ml" />
+                            </Link>
+                        </div>
+                    ))
+                }
+
+            </div>
+        </div>
     )
 }
 
