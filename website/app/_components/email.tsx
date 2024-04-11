48142aedc6517b742fc6b95044b1a1f2eefb7c17
Alessandro <aleferrara1998@gmail.com>
2024-04-11 11:35:09 +0200

    Update email link styling in Email component

--- website/app/_components/email.tsx
+++ website/app/_components/email.tsx
@@ -2,7 +2,7 @@
 
 /**
  * Crawlers don't execute JavaScript. The email is sent as a base64 encoded string and then decoded on the client side.
- * @returns Email component
+ * @returns JSX.Element
  */
 export default function Email() {
     const emailb64 = "YWxlc3NhbmRybzIuZmVycmFyYUBtYWlsLnBvbGltaS5pdA==";
@@ -11,7 +11,7 @@ export default function Email() {
     return (
         <div>
             <h3 className="text-lg font-semibold">Email</h3>
-            <a href={`mailto:${email}`} className="hover:underline">{email}</a>
+            <a href={`mailto:${email}`} className="bg-gradient-to-r from-[#FC540C] to-[#FFD76F] bg-clip-text text-transparent hover:underline">{email}</a>
         </div>
     )
 }
